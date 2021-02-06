9ff9ef3dcbb3de017d81e021a2a93db4b8502376
Soosai Alexander <asoosaialexander@gmail.com>
2021-02-06 18:23:27 +0530

    Find next palindrome

---
+++ Palindrome.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingChallenges
+{
+    public static class Palindrome
+    {
+        /// <summary>
+        /// Given a number, returns the next smallest number which is a palindrome.
+        /// </summary>
+        /// <param name="input">Palindrome Number</param>
+        /// <returns>Next smallest Palindrome</returns>
+        public static long GetNextPalindrome(long num)
+        {
+            if (num < 10)
+            {
+                return num + 1;
+            }
+            while (true)
+            {
+                ++num;                
+                if (Reverse(num) == num)
+                {
+                    return num;
+                }
+            }
+        }
+
+        private static long Reverse(long n)
+        {
+            long rev_num = 0, digit;
+            while (n > 0)
+            {
+                digit = n % 10;
+                rev_num = rev_num * 10 + digit;
+                n /= 10;
+            }
+
+            return rev_num;
+        }
+
+        private static bool IsPalindrome(long num)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProgrammingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] inputList = new long[] { 1239321 };
+            
+            foreach(long input in inputList)
+            {
+                var watch = new System.Diagnostics.Stopwatch();
+
+                watch.Start();
+                long res = Palindrome.GetNextPalindrome(input);
+                watch.Stop();
+
+                Console.WriteLine("Next palindrome of {0} is {1}", input, res);
+                Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+                
+            }
+        }
+    }
+}
